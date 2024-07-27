7f3c717f1bd329d40efb6231cc9676f7b8ae941d
Dinesh Tyagi <dineshtyagi567@gmail.com>
2024-07-28 01:14:25 +0530

    Debugged git case-ignoring

---
+++ src/app/components/Search.jsx
@@ -0,0 +1,67 @@
+"use client"
+import React, { useState } from 'react'
+import {  FaSearch } from 'react-icons/fa';
+import { MdCancel } from "react-icons/md";
+import { Input } from './ui/SimpleInput';
+import { paintings } from './Modal';
+import Image from 'next/image';
+import Modal from './Modal';
+
+const Search = () =>{
+    const [input,setInput] = useState("");
+    const [results,setResults] = useState([]);
+    const [showModal,setShowModal] = useState(false);
+    const [src,setSrc] = useState('');
+
+    const getData=(value)=>{
+        const res = paintings.filter((user)=>{
+            return value && user.description.toLocaleLowerCase().includes(value.toLocaleLowerCase());
+        });
+        setResults(res)
+    }
+
+    const handleChange = (value) =>{
+        setInput(value);
+        getData(value)
+    }
+
+    return(
+        <div className='flex flex-col max-h-24'>
+            <div className='flex'><FaSearch className='mt-2 mr-2' /><Input className='w-64' value={input} onChange={(e)=>handleChange(e.target.value)} placeholder='Enter Search Here' /><MdCancel onClick={()=>handleChange("")} className='text-xl mt-2 ml-2 hover:cursor-pointer' />
+            </div>
+            <div id='results' className='z-40 bg-zinc-100'>
+                {results.map((paint)=>{
+                    return(
+                        <div key={paint.id} onClick={()=>{
+                            setSrc("/"+paint.id+".jpg");
+                            setShowModal(true);
+                        }} className='flex hover:bg-zinc-200 hover:cursor-pointer rounded-lg m-2 p-2'>
+                            <div>
+                            <Image 
+                            alt='painting'
+                            src={"/"+paint.id+".jpg"}
+                            height="50"
+                            width="50"
+                            loading='eager'
+                            priority={true}
+                            className="hover:cursor-grabbing rounded-lg m-2 p-2"
+                            />
+                            </div>
+                            
+                            <div className=' m-2 p-2'>
+                                {paint.title}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+            <Modal isVisible={showModal} src={src} onClose={()=>setShowModal(false)}/>
+        </div>
+    )
+}
+
+export default Search;
+
+
+
+
---
+++ src/app/components/ui/parallax-scroll.tsx
@@ -0,0 +1,110 @@
+"use client";
+import { useScroll, useTransform } from "framer-motion";
+import { useRef,useState } from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { cn } from "../../utils/cn";
+import Modal from '../Modal';
+
+export const ParallaxScroll = ({
+  images,
+  className,
+}: {
+  images: string[];
+  className?: string;
+}) => {
+  const gridRef = useRef<any>(null);
+  const { scrollYProgress } = useScroll({
+    // container: gridRef, // remove this if your container is not fixed height
+    offset: ["start start", "end start"], // remove this if your container is not fixed height
+  });
+
+  const translateFirst = useTransform(scrollYProgress, [0, 1], [0, -200]);
+  const translateSecond = useTransform(scrollYProgress, [0, 1], [0, 200]);
+  const translateThird = useTransform(scrollYProgress, [0, 1], [0, -200]);
+
+  const third = Math.ceil(images.length / 3);
+
+  const firstPart = images.slice(0, third);
+  const secondPart = images.slice(third, 2 * third);
+  const thirdPart = images.slice(2 * third);
+
+  const [showModal,setShowModal] = useState(false);
+  const [src,setSrc] = useState('');
+
+  return (
+    <>
+    <div
+      className={cn("h-[52rem] items-start overflow-y-auto w-full", className)}
+      ref={gridRef}
+    >
+      <div
+        className="md:grid  sm:flex-col md:grid-cols-2 lg:grid-cols-3 items-start  max-w-fit mx-auto gap-10 py-40 px-10"
+        ref={gridRef}
+      >
+        <div className="grid gap-10 relative overflow-hidden">
+          {firstPart.map((el, idx) => (
+            <motion.div
+              style={{ y: translateFirst }} // Apply the translateY motion value here
+              key={"grid-1" + idx}
+            >
+                
+              <Image
+                src={el}
+                className="hover:cursor-grab h-fit object-cover w-full object-left-top  rounded-lg gap-10 !m-0 !p-0"
+                height="400"
+                width="300"
+                alt="thumbnail"
+                onClick={()=>{
+                  setSrc(el);
+                  setShowModal(true);
+                }}
+              />
+            </motion.div>
+          ))}
+        </div>
+        <div className="grid gap-10">
+          {secondPart.map((el, idx) => (
+            <motion.div style={{ y: translateSecond }} key={"grid-2" + idx}>
+                
+              <Image
+                src={el}
+                className="hover:cursor-grab h-fit w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
+                height="400"
+                width="400"
+                alt="thumbnail"
+                onClick={()=>{
+                  setSrc(el);
+                  setShowModal(true);
+                }}
+              />
+              
+            </motion.div>
+          ))}
+        </div>
+        <div className="grid gap-10">
+          {thirdPart.map((el, idx) => (
+            <motion.div style={{ y: translateThird }} key={"grid-3" + idx}>
+                
+              <Image
+                src={el}
+                className="hover:cursor-grab h-fit w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
+                height="400"
+                width="400"
+                alt="thumbnail"
+                onClick={()=>{
+                  setSrc(el);
+                  setShowModal(true);
+                }}
+              />
+              
+            </motion.div>
+          ))}
+        </div>
+        
+      </div>
+    </div>
+    <Modal isVisible={showModal} src={src} onClose={()=>setShowModal(false)}/>
+    </>
+  );
+};
